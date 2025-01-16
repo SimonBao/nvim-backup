7f3dc98685e20590fe9e151b0454721f377ab318
simon <simonbao.sb@gmail.com>
2025-01-16 02:09:16 +0000

    show filename in splits

--- lua/options.lua
+++ lua/options.lua
@@ -11,6 +11,12 @@ opt.spelloptions = "camel" -- Detect camelCase
 opt.spellcapcheck = "" -- Don't check for capital letters at start of sentence
 opt.spellfile = vim.fn.stdpath("config") .. "/spell/en.utf-8.add" -- Custom spell file
 
+-- Add these lines for window titles
+opt.title = true                -- Enable window title
+opt.titlestring = "%t"          -- Show just the file name
+opt.winbar = "%=%m %f"         -- Show filename in window bar
+opt.laststatus = 3             -- Global statusline
+
 -- Create spell directory if it doesn't exist
 local spell_dir = vim.fn.stdpath("config") .. "/spell"
 if vim.fn.isdirectory(spell_dir) == 0 then

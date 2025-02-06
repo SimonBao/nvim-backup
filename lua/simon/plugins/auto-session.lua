aa9370779364e843486790992b3b9f5d05212edd
SimonBao <Simonbao.sb@gmail.com>
2025-02-06 18:26:41 +0000

    update nvim-tree to work better with auto-sessions

--- lua/simon/plugins/auto-session.lua
+++ lua/simon/plugins/auto-session.lua
@@ -4,20 +4,42 @@ return {
   config = function()
     local auto_session = require("auto-session")
 
+    -- Function to update nvim-tree to match current directory
+    local function update_tree()
+      local cwd = vim.fn.getcwd()
+      pcall(function()
+        require("nvim-tree.api").tree.change_root(cwd)
+        require("nvim-tree.api").tree.reload()
+      end)
+    end
+
     auto_session.setup({
-      auto_restore_enabled = false,  -- Disable auto restore temporarily
-      auto_save_enabled = true,      -- Keep auto save enabled
+      auto_restore_enabled = true,   -- Enable auto restore
+      auto_save_enabled = false,     -- Disable auto save
       auto_session_suppress_dirs = { "~/", "~/Downloads", "~/Documents", "~/Desktop/" },
       session_lens = {
-        -- If you don't want the session-lens (telescope plugin for sessions), remove this table
         load_on_setup = true,
         theme_conf = { border = true },
         previewer = false,
+        load_on_select = function(selected)
+          local session_dir = vim.fn.fnamemodify(selected, ":p:h")
+          vim.cmd("cd " .. vim.fn.fnameescape(session_dir))
+        end,
       },
       pre_save_cmds = {
         function()
-          -- Close all nvim-tree windows before saving session
+          -- Close nvim-tree before saving session
           require("nvim-tree.api").tree.close()
+        end,
+      },
+      post_restore_cmds = {
+        function()
+          -- First update the tree root
+          update_tree()
+          -- Then reopen nvim-tree
+          pcall(function()
+            require("nvim-tree.api").tree.open()
+          end)
         end,
       },
     })
